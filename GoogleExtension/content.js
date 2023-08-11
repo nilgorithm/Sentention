8e985a7254e2963b956d73e477208654747d00fa
erzeu <kuznetsovjo@mail.ru>
2023-08-11 15:55:12 +0300

    version 0.3

--- GoogleExtension/content.js
+++ GoogleExtension/content.js
@@ -1,154 +1,71 @@
+function BuildButton() {
+    let button = document.createElement("span");
+    button.setAttribute('class', 'navTabButton');
+    button.setAttribute('id', 'send-button');
+    button.setAttribute('title', 'Show Dynamics CRM send button');
 
-document.documentElement.style.height = '100%';
-document.body.style.height = '100%';
-document.documentElement.style.width = '100%';
-document.body.style.width = '100%';
 
-document.addEventListener('readystatechange', () => console.log(document.readyState));
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("class", "navTabButtonLink");
+    linkElement.setAttribute("title", "");
 
-function bt(){
-    var btn = document.createElement( 'input' );
-    btn.id = 'b-1'
-    btn.type = 'button';
-    btn.value = 'История звонков';
-    btn.style.color = 'white';
-    // btn.style.position = 'absolute';
-    // btn.style.top = '10%';
-    return btn
+    let linkImageContainerElement = document.createElement("span");
+    linkImageContainerElement.setAttribute("class", "navTabButtonImageContainer");
+
+    let imageElement = document.createElement("img");
+    let url_img = chrome.runtime.getURL("images/32_linear.png");
+    imageElement.setAttribute("src", url_img);
+
+    button.setAttribute('style', 'float:left; width:50px; height:50px;cursor:pointer!important');
+    linkElement.setAttribute("style", "float:left; width:50px; height:50px;cursor:pointer!important;text-align:center");
+
+    linkImageContainerElement.appendChild(imageElement);
+    linkElement.appendChild(linkImageContainerElement);
+    button.appendChild(linkElement);
+
+    return button;
 }
 
-function bt_3(){
-    var btn_3 = document.createElement( 'input' );
-    btn_3.id = 'b-3'
-    btn_3.type = 'button';
-    btn_3.value = 'Отправить Уведомление';
-    btn_3.style.color = 'white';
-    return btn_3
-}
+let button = BuildButton();
 
-var btn = bt() 
-var btn_3 = bt_3() 
+function initialize(obj){
+    obj.prepend(button);
 
-// let checkPath = "/html/body/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div[1]/div/div[2]/div/div[1]/span/div[2]/h1"
-let phoneNumberPath = "/html/body/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div[1]/div/div[1]/div[1]/table/tbody/tr[5]/td[2]/div/div[1]/label/a"
-
-function getElementByXpath(path) {
-    return document.evaluate(path, document.getElementById("contentIFrame0").contentWindow.document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
-}
-
-function arr_contains(arr, pred) {
-    for (var i = 0; i < arr.length; i++) {
-        if (typeof pred == 'function' && pred(arr[i], i, arr) || arr[i] === elem) {
-            return true;
-        }
-    }
-    return false;
-}
-
-var container = []
-document.addEventListener("DOMSubtreeModified", function(event){
-    if(document.getElementById("phonecall|NoRelationship|Form|Mscrm.Form.phonecall.Close")) 
-    // if(getElementByXpath(checkPath) == 'Создать объект Звонок')
-    {   
-        if(document.body.contains(btn))
-        {
-            console.log("__exists_btn__")
-            if(!document.body.contains(btn_3))
-            {
-                console.log("__deleted_btn_3__")
-            }
-            else
-            {
-                document.body.removeChild(btn_3)
-            }
-        }
-        else 
-        {   
-            document.body.appendChild( btn );
-            //document.body.appendChild( btn_2 );
-            //document.body.removeChild( btn_3 );
-            //document.body.appendChild( btn_2 );
-            btn.onclick = function () 
-            {   
-                try
-                {   
-                    //var el = getElementByXpath(phoneNumberPath)
-                    // console.log(Xrm.Page.data.entity.getId())
-                    var el = document.getElementById("contentIFrame0").contentWindow.document.getElementById("phonenumber").getElementsByClassName("ms-crm-div-NotVisible ms-crm-div-NotSelectable")[0].textContent
-                    if(arr_contains(container,el))
-                    {
-                        console.log(arr_contains(container,el))
-                        window.onbeforeunload = function() {
-                            return "Требуется перезагрузить страницу и повторить попытку. Перезагрузить?";
-                          };
-                        document.location.reload(true)
+    button.onclick = function (){
+        let frame = document.getElementsByTagName('iframe');
+        for (let i = 0; i<frame.length; i++){
+            if (frame[i].getAttribute("style").indexOf("visibility: visible") != -1){
+                if(frame[i].contentDocument.body.innerHTML.indexOf('Тип задачи Произвести взыскание ДЗ – КЦ') != -1){
+                    try{
+                        let guid = frame[i].contentWindow.document.getElementById("crmFormSubmitId").value;
+                        document.location.href = 'Myprotocol:' + document.getElementById("crmContentPanel").getAttribute("src")+":Form_2";
                     }
-                    else
-                    {
-                        //container.push(el)
-                        //console.log(container)
-                        document.location.href = 'Myprotocol:'+el+":Form_1"
+                    catch{
+                        alert("quid не найден!");
                     }
-                    
                 }
-                catch
-                {   
-                    console.log("Error_1")
-                    window.onbeforeunload = function() {
-                        return "Требуется перезагрузить страницу и повторить попытку. Перезагрузить?";
-                      };
-                    document.location.reload(true)
+                else{
+                    alert("Кнопка предназначена для вкладки 'Произвести взыскание!'");
                 }
             }
         }
     }
-    else if(document.getElementById("task|NoRelationship|Form|gpbl.task.Collection.TaskForm.Button"))
-    {
-        if(document.body.contains(btn_3))
-        {
-            console.log("__exists_btn3__")
-            if(!document.body.contains(btn))
-            {
-                console.log("__deleted_btn__")
-            }
-            else
-            {
-                document.body.removeChild()
-            }
-        }
-        else
-        {
-            document.body.appendChild( btn_3 );
-            try
-            {
-                btn_3.onclick = function (){
-                    document.location.href = 'Myprotocol:'+document.getElementById("crmContentPanel").getAttribute("src")+":Form_2"
-                }
-            }
-            catch
-            {
-                document.location.reload(true)     
-            }
-        }
-    }
-    
+}
+
+
+// set up the mutation observer
+var observer = new MutationObserver(function (mutations, me) {
+  var obj = document.getElementById('navBar');
+  if (obj) {
+    initialize(obj);
+    me.disconnect(); // stop observing
+    return;
+  }
 });
 
 
-
-// //наблюдение за изменениями:
-// var mutationObserver = new MutationObserver(function(mutations) {
-//     mutations.forEach(function(mutation) {
-//       console.log(mutation);
-//     });
-// });
-
-// // Запускаем наблюдение за изменениями в корневом HTML-элементе страницы
-// mutationObserver.observe(document.documentElement, {
-//     attributes: true,
-//     characterData: true,
-//     childList: true,
-//     subtree: true,
-//     attributeOldValue: true,
-//     characterDataOldValue: true
-// });
+// start observing
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
